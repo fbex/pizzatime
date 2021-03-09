ac91c3a281a7d632df763befb48b96acb8c69e8c
Florian Bernhardt <fbex@posteo.de>
2021-03-09 20:20:24 +0100

    Define structure of the website

---
+++ src/components/Navbar.vue
@@ -0,0 +1,64 @@
+<template>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="https://bulma.io">
+          <img
+            src="https://bulma.io/images/bulma-logo.png"
+            width="112"
+            height="28"
+          />
+        </a>
+
+        <a
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+          <router-link class="navbar-item" to="/">Home</router-link>
+          <router-link class="navbar-item" to="/about">About</router-link>
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link">Recipes</a>
+            <div class="navbar-dropdown">
+              <a class="navbar-item">Poolish (Vito Iacopelli)</a>
+              <a class="navbar-item">Direct Dough (Vincenzo)</a>
+              <a class="navbar-item">Other</a>
+            </div>
+          </div>
+        </div>
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <a
+                class="button"
+                href="https://github.com/fbex/pizzatime"
+                target="_blank"
+              >
+                <span class="icon"><i class="fab fa-github"></i></span>
+                <span>GitHub</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Navbar"
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <section class="section">
+    <div class="container">
+      <HomeComponent msg="This will be the Home" />
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import HomeComponent from "@/components/HomeComponent.vue"; // @ is an alias to /src
 
 export default defineComponent({
   name: "Home",
   components: {
-    HelloWorld
+    HomeComponent
   }
 });
 </script>
