70a27ce84011c033dfdc24c2dc991714e834a6a6
Florian Bernhardt <fbex@posteo.de>
2021-03-19 23:45:38 +0100

    Update about page

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -4,10 +4,10 @@
       <div class="content has-text-centered">
         <p>
           <strong>Pizzatime</strong> by
-          <a href="https://github.com/fbex">fbex</a>
-          . The source code is licensed
-          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>
-          . The website content is licensed
+          <a href="https://github.com/fbex">fbex</a>. The source code is
+          licensed
+          <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The
+          website content is licensed
           <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
             >CC BY NC SA 4.0</a
           >.
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,18 +1,35 @@
 <template>
   <section class="section">
     <div class="container">
-      <h1 class="title">This is an about page</h1>
-      <div>
-        Logo icon made by
-        <a
-          href="https://www.flaticon.com/de/autoren/pixelmeetup"
-          title="Pixelmeetup"
-          >Pixelmeetup</a
-        >
-        from
-        <a href="https://www.flaticon.com/de/" title="Flaticon"
-          >www.flaticon.com</a
-        >
+      <div class="content">
+        <h1 class="title">About Pizzatime</h1>
+        <p>
+          This site is a non-commercial side project of mine.<br />
+          If you run into any problems or have ideas to improve the site, feel
+          free to contact me on
+          <a
+            href="https://github.com/fbex/pizzatime/issues/new"
+            title="GitHub issue"
+            >GitHub</a
+          >
+          or
+          <a href="https://github.com/fbex/pizzatime" title="GitHub pizzatime"
+            >fork Pizzatime</a
+          >
+          on your own.
+        </p>
+        <p>
+          Logo icon made by
+          <a
+            href="https://www.flaticon.com/de/autoren/pixelmeetup"
+            title="Pixelmeetup"
+            >Pixelmeetup</a
+          >
+          from
+          <a href="https://www.flaticon.com/de/" title="Flaticon"
+            >www.flaticon.com</a
+          >
+        </p>
       </div>
     </div>
   </section>
