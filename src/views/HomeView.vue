7d7d1aef85f9295dbe5ff538ec01d1b06c0d0d72
fbex <8228317+fbex@users.noreply.github.com>
2024-01-30 20:56:19 +0100

    Change landing page layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,16 @@
 <template>
   <section class="hero is-primary is-fullheight-with-navbar">
     <div class="hero-body">
-      <div>
+      <div class="container has-text-centered">
+        <img src="@/assets/pizza.png" width="64" height="64" alt="Pizza icon"/>
         <p class="title">
           Pizzatime
         </p>
-        <p class="subtitle">
-          Easily calculate your favorite pizza recipes
-        </p>
+        <p class="subtitle">The pizza dough calculator</p>
+        <button class="button is-link" @click="$router.push('calculator')">Get Started!</button>
       </div>
     </div>
   </section>
 </template>
+<script setup lang="ts">
+</script>
