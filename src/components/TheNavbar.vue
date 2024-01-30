70f45a828ea98d668a8dff5895b8cd13d693c311
fbex <8228317+fbex@users.noreply.github.com>
2024-01-30 20:23:21 +0100

    Add support for navbar burger menu on touch devices

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -3,15 +3,17 @@
     <div class="container">
       <div class="navbar-brand">
         <router-link class="navbar-item" to="/">
-          <img src="@/assets/logo.png" width="112" height="28" />
+          <img src="@/assets/logo.png" width="112" height="28"/>
         </router-link>
 
         <a
-          role="button"
-          class="navbar-burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
+            role="button"
+            class="navbar-burger"
+            :class="burgerMenuActive ? 'is-active' : ''"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarItems"
+            @click="toggleBurgerMenu"
         >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
@@ -19,20 +21,25 @@
         </a>
       </div>
 
-      <div class="navbar-menu">
+      <div id="navbarItems" class="navbar-menu" :class="burgerMenuActive ? 'is-active' : ''">
         <div class="navbar-start">
-          <router-link class="navbar-item" to="/calculator"
-            >Dough Calculator</router-link
+          <router-link
+              class="navbar-item"
+              to="/calculator"
+              @click="closeBurgerMenu"
+          >Dough Calculator
+          </router-link
           >
-          <router-link class="navbar-item" to="/about">About</router-link>
+          <router-link class="navbar-item" to="/about" @click="closeBurgerMenu">About</router-link>
         </div>
         <div class="navbar-end">
           <div class="navbar-item">
             <div class="buttons">
               <a
-                class="button"
-                href="https://github.com/fbex/pizzatime"
-                target="_blank"
+                  class="button"
+                  href="https://github.com/fbex/pizzatime"
+                  target="_blank"
+                  @click="closeBurgerMenu"
               >
                 <span class="icon"><i class="fab fa-github"></i></span>
                 <span>GitHub</span>
@@ -44,3 +51,24 @@
     </div>
   </nav>
 </template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "TheNavbar",
+  data() {
+    return {
+      burgerMenuActive: false
+    }
+  },
+  methods: {
+    toggleBurgerMenu() {
+      this.burgerMenuActive = !this.burgerMenuActive;
+    },
+    closeBurgerMenu() {
+      this.burgerMenuActive = false;
+    }
+  }
+})
+</script>
