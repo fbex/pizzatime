aa884b7d40e590205ea3d8ab371f1bcb3575c0b8
fbex <8228317+fbex@users.noreply.github.com>
2024-01-31 13:40:09 +0100

    Add a catch-all route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 import CalculatorView from "@/views/CalculatorView.vue";
 
@@ -22,7 +22,13 @@ const router = createRouter({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue")
-    }
+    },
+    // will match everything and put it under `$route.params.pathMatch`
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: HomeView
+    },
   ]
 });
 
